866beeed1af04b57e28b34fd7223ee174ea70626
zeph <drivelecture@gmail.com>
2022-01-06 15:37:34 +0530

    password fix in env

--- main.py
+++ main.py
@@ -1,10 +1,14 @@
+import os
 from tkinter import *
 from PIL import ImageTk,Image #PIL -> Pillow
-import pymysql
-from AddBook import *
-from DeleteBook import *
-from ViewBooks import *
-from IssueBook import *
-
+# import pymysql
+# from AddBook import *
+# from DeleteBook import *
+# from ViewBooks import *
+# from IssueBook import *
+from dotenv import load_dotenv
 # Add your own database name and password here to reflect in the code
- 
+load_dotenv()
+
+mysqldbPassword = os.getenv("Password")
+print(mysqldbPassword)
