b6b62811e8b06ea2ec238f7de9bea58d616fe66f
Akshay <akshaysingh48641@gmail.com>
2022-01-06 15:27:38 +0530

    inserts main.py

---
+++ main.py
@@ -0,0 +1,10 @@
+from tkinter import *
+from PIL import ImageTk,Image #PIL -> Pillow
+import pymysql
+from AddBook import *
+from DeleteBook import *
+from ViewBooks import *
+from IssueBook import *
+
+# Add your own database name and password here to reflect in the code
+ 
