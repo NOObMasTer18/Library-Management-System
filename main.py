be261689b2fa6ddd8360a375869bc38f2d3420e1
Hash <drivelecture@gmail.com>
2022-01-10 13:41:42 +0530

    background photo set

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from pathlib import Path
 # from methods.ViewBooks import *
 # from methods.IssueBook import *
 #extras
-from Images.images import getLibImage
+from Images.images import set_backgroundImage,getLibImage
 
 #init
 from init import get_cursor
@@ -23,9 +23,23 @@ root.title("Library")
 root.minsize(width=400,height=400) 
 root.geometry("600x500")
 
-# setting image
 imgPath = getLibImage()
-background_Image = Image.open(imgPath)
+same=True
+n=0.25
+# Adding a background image
+background_image =Image.open(imgPath)
+[imageSizeWidth, imageSizeHeight] = background_image.size
+newImageSizeWidth = int(imageSizeWidth*n)
+if same:
+    newImageSizeHeight = int(imageSizeHeight*n) 
+else:
+    newImageSizeHeight = int(imageSizeHeight/n) 
+
+img = ImageTk.PhotoImage(background_image)
+Canvas1 = Canvas(root)
+Canvas1.create_image(300,340,image = img)      
+Canvas1.config(bg="white",width = newImageSizeWidth, height = newImageSizeHeight)
+Canvas1.pack(expand=True,fill="both")
 
 
 root.mainloop() #run tkinter
